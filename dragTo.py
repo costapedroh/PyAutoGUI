977bf91030bed04630e4e344ac97e25b1f839a98
Pedro Costa <costa.pedroh78@gmail.com>
2025-02-03 18:21:12 -0300

    arrastar e soltar

---
+++ dragTo.py
@@ -0,0 +1,7 @@
+import pyautogui
+from time import sleep
+
+pyautogui.moveTo(940,356, duration=1.5)
+pyautogui.dragTo(1318,643, duration=2.5, button='left')
+pyautogui.moveTo(1140,495, duration=1.5)
+pyautogui.dragTo(820,416, duration=2.5, button='left')
